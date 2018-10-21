d608e66be4b0f0b208f9e22ed99dd0de71687c4a
christianrorholtmoe <34798018+christianrorholtmoe@users.noreply.github.com>
2018-10-21 17:38:09 +0000

    S

---
+++ backend/src/main/java/alexa/backend/AlexaBackendController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package alexa.backend;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author christian
+ *
+ */
+public class AlexaBackendController {
+
+	
+	
+}
