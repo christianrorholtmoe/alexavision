061f7c9d713cf8220ee76a168b7df797c0aa97c8
christianrorholtmoe <34798018+christianrorholtmoe@users.noreply.github.com>
2018-10-30 11:50:17 +0000

    F comm

---
+++ voiceinterface/src/main/java/alexavision/voiceinterface/App.java
@@ -0,0 +1,13 @@
+package alexavision.voiceinterface;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
