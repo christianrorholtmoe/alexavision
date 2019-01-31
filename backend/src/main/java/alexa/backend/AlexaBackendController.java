a92930fd75d6a768767517f97381e32ddd1825e1
christianrorholtmoe <34798018+christianrorholtmoe@users.noreply.github.com>
2019-01-31 21:42:14 +0000

    s

--- backend/src/main/java/alexa/backend/AlexaBackendController.java
+++ backend/src/main/java/alexa/backend/AlexaBackendController.java
@@ -16,7 +16,7 @@ public class AlexaBackendController {
 
 	
 	//http://localhost:8080/describeImage?url=http://e24.vgc.no/drpublish/images/article/2015/11/13/23560926/1/default/Anita.jpg
-	//https://alexavision.azurewebsites.net/getImagedescription
+	//https:/ /alexavision.azurewebsites.net/getImagedescription
 	//https://github.com/kasperkamperman/MobileCameraTemplate.git
 	
 	//class vars
